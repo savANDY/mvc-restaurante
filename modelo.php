08e68cdd36246c2e3a49cb75bd5dd5a835af9151
savANDY <andicuu@gmail.com>
2017-10-26 14:41:55 +0200

    Formato parentesis {}

--- modelo.php
+++ modelo.php
@@ -1,26 +1,21 @@
 <?php
-class Platillo
-{
+class Platillo {
     private $platillo;
     private $dbh;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->platillo = array();
-        $this->dbh = $link = new mysqli('localhost', 'root', '', 'restaurante');
+        $this->dbh = new mysqli('localhost', 'root', '', 'restaurante');
     }
 
-    private function set_names()
-    {
+    private function set_names() {
         return $this->dbh->query("SET NAMES 'utf8'");
     }
 
-    public function lista_platillos()
-    {
+    public function lista_platillos() {
         self::set_names();
         $sql="select nombre, precio from platillos where disponible = 1";
-        foreach ($this->dbh->query($sql) as $res)
-        {
+        foreach ($this->dbh->query($sql) as $res) {
             $this->platillo[]=$res;
         }
         return $this->platillo;
