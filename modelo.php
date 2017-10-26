db61cd3ababba58bef3007b4a33e189789c30cae
savANDY <andicuu@gmail.com>
2017-10-26 14:49:41 +0200

    Cambio variable $link

--- modelo.php
+++ modelo.php
@@ -1,25 +1,25 @@
 <?php
 class Platillo {
     private $platillo;
-    private $dbh;
+    private $link;
 
     public function __construct() {
         $this->platillo = array();
-        $this->dbh = new mysqli('localhost', 'root', '', 'restaurante');
+        $this->link = new mysqli('localhost', 'root', '', 'restaurante');
     }
 
     private function set_names() {
-        return $this->dbh->query("SET NAMES 'utf8'");
+        return $this->link->query("SET NAMES 'utf8'");
     }
 
     public function lista_platillos() {
         self::set_names();
         $sql="select nombre, precio from platillos where disponible = 1";
-        foreach ($this->dbh->query($sql) as $res) {
+        foreach ($this->link->query($sql) as $res) {
             $this->platillo[]=$res;
         }
         return $this->platillo;
-        $this->dbh=null;
+        $this->link=null;
     }
 }
 ?>
